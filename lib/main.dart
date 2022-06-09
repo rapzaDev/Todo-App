fc5e376bf1332539d0e778bf389f4f4c23027661
rapzaDev <rapzadev@gmail.com>
2022-06-08 23:42:18 -0300

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Todo App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.brown,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Todo App'),
+      ),
+      body: Container(
+        child: const Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/models/item.dart
@@ -0,0 +1,20 @@
+// ignore_for_file: unnecessary_this, prefer_collection_literals, unnecessary_new
+
+class Item {
+  late String title;
+  late bool done;
+
+  Item({required this.title, required this.done});
+
+  Item.fromJson(Map<String, dynamic> json) {
+    title = json['title'];
+    done = json['done'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['title'] = this.title;
+    data['done'] = this.done;
+    return data;
+  }
+}
